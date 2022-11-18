64e409d7f6825c67693ba2244f222dccb0fc91a0
grimma2 <80467627+grimma2@users.noreply.github.com>
2022-11-18 20:41:18 +0500

    reome 'collectstatic' from build file

--- build.sh
+++ build.sh
@@ -7,6 +7,5 @@ set -o errexit
 
 python -m ensurepip --default-pip
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsuperuser --email chella05andrey@gmail.com --noinput
