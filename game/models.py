c05dcd0fa1412ad9c862b98ae062a21238858593
grimma2 <80467627+grimma2@users.noreply.github.com>
2022-11-18 21:56:20 +0500

    Add files via upload

--- game/models.py
+++ game/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
 
 class Game(models.Model):
@@ -26,3 +27,34 @@ class Question(models.Model):
     text = models.TextField('Текст вопроса')
     order = models.PositiveSmallIntegerField(default=1)
     correct_answers = models.JSONField('Правильные ответы на вопрос', default=list)
+
+    class Meta:
+        ordering = ['order']
+
+
+class LeaderBoard(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.SET_NULL, null=True, related_name='leader_boards')
+    start_time = models.DateTimeField('Дата начала игры', default=timezone.now)
+    end_date = models.DateTimeField('Дата окончания игры', blank=True, null=True)
+    already_end = models.BooleanField('Игра окончена', default=False)
+
+    def finish(self):
+        self.end_date = timezone.now()
+        self.already_end = True
+
+        for team in self.game.team_set.all():
+            FinishTeam.objects.get_or_create(
+                team=team, leader_board=self, bonus_points=team.bonus_points
+            )
+
+        self.save()
+        self.refresh_from_db()
+
+
+class FinishTeam(models.Model):
+    team = models.ForeignKey('team.Team', on_delete=models.SET_NULL, null=True)
+    leader_board = models.ForeignKey(LeaderBoard, on_delete=models.CASCADE)
+    finish_date = models.DateTimeField(
+        'Дата и время финиша команды', default=timezone.now
+    )
+    bonus_points = models.PositiveIntegerField('Бонусные баллы', default=0)
--- quichannels/utils.py
+++ quichannels/utils.py
@@ -3,19 +3,19 @@ import logging
 from datetime import datetime, date
 from dataclasses import dataclass
 
-from celery.contrib.abortable import AbortableAsyncResult
 from asgiref.sync import async_to_sync
 from django.db.models import F
 
-from .tasks import set_timer
+from .tasks import set_timer, send_state_to_consumer
 
-from quiz.celery import app as celery_app
+from quiz.settings import SECRET_KEY
 
 from team.models import Team, Timer
-from team.utils import get_team_question, get_leader_board
+from team.utils import get_team_question
 
-from game.models import Game
+from game.models import Game, FinishTeam, LeaderBoard
 from game.serializers import QuestionSerializer
+from game.utils import LeaderBoardFetcher
 
 
 def code_is_valid(team: Team) -> None:
@@ -23,30 +23,49 @@ def code_is_valid(team: Team) -> None:
         raise Exception('Team with this code not found')
 
 
-def clear_team_active_question(game: Game):
+def game_off_team_basics(game: Game, revoke_timers=True) -> None:
     for team in game.team_set.all():
         team.active_question = 0
+        team.bonus_points = 0
+
+        if revoke_timers:
+            if team.timer:
+                team.save()
+                team.timer.delete()
+                return
+
         team.save()
 
 
-def change_game_state(game: Game, state) -> None:
+def all_teams_finished(finished_team_question: int, game: Game) -> bool:
+    for team in game.team_set.all():
+        if team.active_question != finished_team_question:
+            break
+    else:
+        return True
+
+
+def change_game_state(game: Game, state, revoke_timers=True) -> None:
     print('change_game_state')
-    logging.getLogger('DL').info('change_game_state')
     game.game_state = state
     game.save()
     dependency = GameTimersDependency(game=game)
 
     if state == 'OFF':
-        clear_team_active_question(game)
-        dependency.revoke_timers()
+        game_off_team_basics(game, revoke_timers)
+        LeaderBoardFetcher(game=game).board.finish()
     else:
+        LeaderBoard.objects.create(game=game)
         dependency.set_timers()
 
 
 class GroupMessageSender:
 
-    def send_to_all(self, game: Game, send: dict) -> None:
-        for team in game.team_set.all():
+    def send_to_all(self, game: Game, send: dict, *, exclude_code=None) -> None:
+        teams = (
+            game.team_set.all().exclude(code=exclude_code) if exclude_code else game.team_set.all()
+        )
+        for team in teams:
             async_to_sync(self.channel_layer.group_send)(
                 f'{team.code}_team', send
             )
@@ -60,7 +79,6 @@ class GameTimersDependency:
         print('set_timers')
         logging.getLogger('DL').info('set_timers...')
         ques_time = datetime.combine(date.min, self.game.question_time) - datetime.min
-        print(f'{ques_time=}')
         for team in self.game.team_set.all():
             print(f'{team.code=}')
             task = set_timer.apply_async(args=[ques_time.total_seconds(), team.code])
@@ -75,28 +93,32 @@ class GameTimersDependency:
             if not team.timer:
                 return
 
-            print('before revoke')
-            AbortableAsyncResult(team.timer.task_id, app=celery_app).abort()
             team.timer.delete()
-            print('after revoke')
 
 
 class NextQuestionSender(GroupMessageSender):
 
-    def send_to_next_question(self, team: Team):
+    def send_to_next_question(self, team: Team, bonus_points: int):
+        event_data = self.get_next_question(team, bonus_points)
+
+        if not event_data:
+            # not send next_question if we don't need event_data and this is None
+            return
+
         async_to_sync(self.channel_layer.group_send)(
             f'{team.code}_team',
             {
                 'type': 'next_question',
-                'event_data': self.get_next_question(team),
+                'event_data': event_data,
             }
         )
 
-    def get_next_question(self, team: Team):
+    def get_next_question(self, team: Team, bonus_points):
         team.active_question = F('active_question') + 1
+        team.bonus_points = F('bonus_points') + bonus_points
         team.save()
-        team.refresh_from_db()
-        questions = team.game.question_set.order_by('order')
+        team.refresh_from_db(fields=['active_question'])
+        questions = team.game.question_set.all()
 
         question = get_team_question(team.active_question, questions)
 
@@ -109,17 +131,42 @@ class NextQuestionSender(GroupMessageSender):
             print('delete timer')
             team.timer.delete()
 
-            leader_board = get_leader_board(team.game)
+            fetcher = LeaderBoardFetcher(game=team.game)
+            return self.send_to_leader_board(fetcher, team)
+
+    def send_to_leader_board(self, fetcher: LeaderBoardFetcher, team: Team):
+        FinishTeam.objects.create(
+            team=team, leader_board=fetcher.board, bonus_points=team.bonus_points
+        )
+
+        if all_teams_finished(team.active_question, team.game):
+            print('all_teams_finished')
+            # turn off game if all teams are finished
+            change_game_state(team.game, state='OFF', revoke_timers=False)
+            send_state_to_consumer.apply_async(args=['OFF', team.game.pk])
+            self.send_to_all(team.game, {'type': 'change_state', 'event_data': Game.GameState.OFF})
+        else:
+            parsed_data = fetcher.parse()
             self.send_to_all(
-                team.game, {'type': 'update_leader_board', 'leader_board': leader_board}
+                team.game,
+                {'type': 'update_leader_board', 'event_data': parsed_data},
+                exclude_code=team.code
             )
-            return leader_board
+            return parsed_data
 
 
 class UpdateLeaderBoardEvent:
 
     def update_leader_board(self, event):
+        print(event)
         self.send(text_data=json.dumps({
             'event': 'update_leader_board',
-            'event_data': event['leader_board'],
+            'event_data': event['event_data'],
         }))
+
+
+class SecretKeyValidation:
+
+    def validate_connect(self):
+        if not self.scope['url_route']['kwargs']['secret_key'] == SECRET_KEY:
+            raise Exception('invalid SECRET_KEY')
