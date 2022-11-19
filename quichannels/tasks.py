d87f0ac4a9647092b5f052b8c895c6e8f7e19edd
grimma2 <80467627+grimma2@users.noreply.github.com>
2022-11-19 10:15:46 +0500

    Update tasks.py

--- quichannels/tasks.py
+++ quichannels/tasks.py
@@ -1,7 +1,40 @@
+import asyncio
+import websockets
+import json
 import time
+
 from celery.contrib.abortable import AbortableTask
 from quiz.celery import app
-from game.utils import WebsocketSenders
+from quiz.settings import SECRET_KEY
+
+
+class WebsocketSenders:
+    HOST = 'localhost:8000'
+    LOOP = asyncio.get_event_loop()
+
+    @classmethod
+    def send_code(cls, code: str) -> None:
+
+        async def code_sender():
+            async with websockets.connect(f'wss://{cls.HOST}/timer/{SECRET_KEY}/') as websocket:
+                await websocket.send(code)
+                await websocket.recv()
+
+        cls.LOOP.run_until_complete(code_sender())
+
+    @classmethod
+    def send_state(cls, state, game_pk):
+        connection_url = f'ws://{cls.HOST}/game-change-state/{SECRET_KEY}/'
+
+        async def state_sender():
+            async with websockets.connect(connection_url) as websocket:
+                await websocket.send(json.dumps({
+                    'pk': game_pk,
+                    'event_data': state
+                }))
+                await websocket.recv()
+
+        cls.LOOP.run_until_complete(state_sender())
 
 
 @app.task(bind=True, base=AbortableTask)
@@ -19,3 +52,4 @@ def set_timer(self, time_for: float, code):
 @app.task
 def send_state_to_consumer(state: str, game_pk: int):
     WebsocketSenders.send_state(state=state, game_pk=game_pk)
+
