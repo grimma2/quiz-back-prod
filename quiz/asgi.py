16816643a94e6cc60c571a664cfe81ef54a46f57
grimma2 <80467627+grimma2@users.noreply.github.com>
2022-11-21 00:08:01 +0500

    up django setup

--- quiz/asgi.py
+++ quiz/asgi.py
@@ -4,12 +4,15 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+
+# place setup before fetch app code what ain't done for this
+django.setup()
+
 import quichannels.routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
-django.setup()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
